6e1311b5df55f31681252429ea45e95ff275b782
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-11-08 19:24:27 -0600

    feat: adición de clase portfolio

---
+++ src/spt.domain/portfolio.cpp
@@ -0,0 +1,12 @@
+export module spt.domain:portfolio;
+
+import std;
+
+namespace spt::domain::investments {
+    export class Portfolio final {
+        private:
+
+        public:
+
+    };
+}
--- src/spt.domain/spt.domain.ixx
+++ src/spt.domain/spt.domain.ixx
@@ -4,3 +4,4 @@ export import :price;
 export import :pricepoint;
 export import :transaction;
 export import :company;
+export import :portfolio;
