508efd0c31d83faa4a4b2835785dd41dedfc813b
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-11-08 18:04:48 -0600

    feat: adición de clase para mantener información de transacciones de compra y venta

--- src/spt.domain/spt.domain.ixx
+++ src/spt.domain/spt.domain.ixx
@@ -1,3 +1,4 @@
 export module spt.domain;
 
 export import :pricepoint;
+export import :transaction;
---
+++ src/spt.domain/transaction.cpp
@@ -0,0 +1,56 @@
+export module spt.domain:transaction;
+
+import std;
+
+namespace spt::domain::investments {
+    using std::chrono::system_clock;
+    using std::format;
+    using std::invalid_argument;
+
+    export enum class TransactionType {
+        Buy, Sell
+    };
+
+    export class Transaction {
+        private:
+            TransactionType _type;
+            int _shares;
+            double _unitPrice;
+            system_clock::time_point _stamp;
+
+        public:
+            Transaction(TransactionType type, int shares, double unitPrice) 
+                : _type { type },
+                  _shares { shares },
+                  _unitPrice { unitPrice },
+                  _stamp { system_clock::now() }
+            {
+                if (shares <= 0) {
+                    throw invalid_argument { "Share count cannot be zero or negative" };
+                }
+                if (unitPrice <= 0.001) {
+                    throw invalid_argument { "Share unit price cannot be less than one cent" };
+                }
+            }
+
+            TransactionType getType() const {
+                return _type;
+            }
+
+            int getShares() const {
+                return _shares;
+            }
+
+            double getUnitPrice() const {
+                return _unitPrice;
+            }
+
+            double getTotalValue() const {
+                return _shares * _unitPrice;
+            }
+
+            system_clock::time_point getStamp() const {
+                return _stamp;
+            }
+    };
+}
