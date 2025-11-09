7b45a320b0474486096379fd2217e7b3b017eefb
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-11-08 18:28:10 -0600

    feat: clase base para información de empresas

---
+++ src/spt.domain/company.cpp
@@ -0,0 +1,42 @@
+export module spt.domain:company;
+
+import std;
+import :transaction;
+import :pricepoint;
+
+namespace spt::domain::investments {
+    using std::invalid_argument;
+    using std::string;
+    using std::vector;
+    using spt::domain::investments::Transaction;
+    using spt::domain::investments::PricePoint;
+
+    export class Company final {
+        private:
+            string _symbol;
+            string _name;
+            vector<Transaction> _transactions;
+            vector<PricePoint> _pricePoints;
+
+        public:
+            Company(const string& symbol, const string& name)
+                : _symbol { symbol },
+                  _name { name },
+                  _transactions { },
+                  _pricePoints { }
+            {
+                if (symbol.empty()) {
+                    throw invalid_argument { "The company ticker symbol cannot be empty" };
+                }
+                if (name.empty()) {
+                    throw invalid_argument { "The company must have a valid name" };
+                }
+            }
+
+            void changePricePoint(double newPrice) {
+                if (newPrice < 0.01) {
+                    throw invalid_argument { "Price cannot be less than one cent" };
+                }
+            }
+    };
+}
--- src/spt.domain/spt.domain.ixx
+++ src/spt.domain/spt.domain.ixx
@@ -1,4 +1,6 @@
 export module spt.domain;
 
+export import :price;
 export import :pricepoint;
 export import :transaction;
+export import :company;
